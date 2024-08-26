12134cc6cdd2e056b6cfc6bb5d7f8539dbfbda3c
boham13@naver.com <boham13@naver.com>
2024-08-26 22:24:38 +0900

    groomlevel_level1

--- groomlevel/groomlevel_basic.py
+++ groomlevel/groomlevel_basic.py
@@ -10,3 +10,32 @@ for i in range(2,N+1):
 		result += sqrt * sqrt
 
 print(result)
+
+### 의좋은 형제
+
+first, second = map(int,input().split())
+N = int(input())
+time = 1
+
+while time <= N:
+	if time % 2 != 0:
+		if first % 2 != 0:
+			give = first // 2 + 1
+		else:
+			give = first // 2
+		
+		second += give
+		first -= give
+		
+	else:
+		if second % 2 != 0:
+			give = second // 2 + 1
+		else:
+			give = second // 2
+		
+		first += give
+		second -= give
+	
+	time += 1
+				
+print(str(first) + ' ' + str(second))
