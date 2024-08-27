a11e9c5e4448697eeacd143de2b8ced33731f331
boham13@naver.com <boham13@naver.com>
2024-08-27 23:04:44 +0900

    baekjoon_easylevel

--- baekjoon/easy_level.py
+++ baekjoon/easy_level.py
@@ -4,3 +4,22 @@
 first,second = map(int,input().split())
 
 print(first + second)
+
+### A / B
+
+up, down = map(int,input().split())
+
+print(up / down)
+
+
+### 오븐 시계
+
+hour, min = map(int, input().split())
+min_plus = int(input())
+
+hour += (min + min_plus) // 60
+min = (min + min_plus) % 60
+
+hour = hour % 24
+
+print(hour, min)
