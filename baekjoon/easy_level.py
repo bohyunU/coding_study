91b5bb100c2424d11ba5e942231c8ab0f2010fad
boham13@naver.com <boham13@naver.com>
2024-08-28 23:03:38 +0900

    baekjoon_easylevel

--- baekjoon/easy_level.py
+++ baekjoon/easy_level.py
@@ -23,3 +23,29 @@ min = (min + min_plus) % 60
 hour = hour % 24
 
 print(hour, min)
+
+
+### 팰린드롬인지 확인하기
+
+word = input()
+
+temp = list(word)
+first = ''
+second = ''
+if len(word) % 2 == 0:    
+    for i in range(0,len(temp)//2):
+        first = first + temp[i]
+
+    for i in range(len(temp)-1,len(temp)//2-1, -1):
+        second = second + temp[i]
+else:
+    for i in range(0,len(temp)//2):
+        first = first + temp[i]
+
+    for i in range(len(temp)-1,len(temp)//2, -1):
+        second = second + temp[i]
+
+if first == second:
+    print(1)
+else:
+    print(0)
