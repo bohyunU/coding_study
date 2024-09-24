44c5f29d19f381030c0d1ba5133b374e59829262
boham13@naver.com <boham13@naver.com>
2024-09-24 23:59:17 +0900

    baekjoon_normal_level

--- baekjoon/normal_level.py
+++ baekjoon/normal_level.py
@@ -26,3 +26,22 @@ else:
     print(sum(result))
     print(min(result))
     
+
+## 첼시를 도와줘
+
+N = int(input())
+
+# 각 테스트 케이스마다 처리
+for i in range(N):
+    # 고려해야할 선수의 수 입력
+    M = int(input())
+    player_dict = {}
+    
+    # 선수 정보 입력
+    for j in range(M):
+        sal, name = input().split()
+        player_dict[int(sal)] = name
+    
+    # 가장 비싼 선수의 이름 출력
+    print(player_dict[max(player_dict)])
+    
