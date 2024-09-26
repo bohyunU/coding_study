c0a160cf04bc8d81fb9577247be44af40a366ca6
boham13@naver.com <boham13@naver.com>
2024-09-27 00:39:20 +0900

    baekjoon_normal_level

--- baekjoon/normal_level.py
+++ baekjoon/normal_level.py
@@ -45,3 +45,22 @@ for i in range(N):
     # 가장 비싼 선수의 이름 출력
     print(player_dict[max(player_dict)])
     
+    
+    
+## 생일
+
+N = int(input())
+
+people_list = {}
+for i in range(N):
+    name, day, mon, year = input().split()
+    if len(mon) == 1:
+        mon = '0' + mon
+    if len(day) == 1:
+        day = '0' + day
+    birth = int(year + mon + day)
+    people_list[birth] = name
+
+print(people_list[max(people_list)])
+print(people_list[min(people_list)])
+    
